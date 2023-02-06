a57e2d28a61368bc4e090cd1dc6361a48eda9ef2
root <root@localhost>
2023-02-06 13:42:04 +0000

    procfile + rroutes + app.json

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 root "books#index"
-  resources :books
+resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
